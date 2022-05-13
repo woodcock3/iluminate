429991f9dbc8bc08b465bc62ff6a830eea4c8cea
Paul <64870518+woodcock3@users.noreply.github.com>
2022-05-13 19:10:52 +0100

    Create _processor.ts

---
+++ _processor.ts
@@ -0,0 +1,24 @@
+// @ts-nocheck
+import * as esbuild from "https://deno.land/x/esbuild@v0.12.17/mod.js";
+import { siteDest, siteSrc } from "./_config.ts";
+
+
+export async function js(page: any) {
+
+  let { path, ext } = page.src;
+  let entryPoint = path + ext;
+  let filename = path.split(/[\\\/]/).pop();
+  let outfile = filename + ".min" + ext;
+
+  return esbuild
+    .build({
+      entryPoints: [siteSrc + entryPoint],
+      outfile: siteDest + "/" + outfile,
+      bundle: true,
+      minify: true,
+    })
+    .then(() => {
+      console.log("🥡 Bundle", outfile);
+      esbuild.stop();
+    })
+}
