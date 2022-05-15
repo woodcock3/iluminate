ecc7dd44cf7176e2f6533f690d95e97a050787c7
Paul <64870518+woodcock3@users.noreply.github.com>
2022-05-15 17:25:42 +0100

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export {default as alpine} from 'https://cdn.skypack.dev/alpinejs?dts';
-export {default as partytown} from 'https://cdn.skypack.dev/@builder.io/partytown?dts';
+export {default as alpine} from 'https://cdn.skypack.dev/alpinejs';
+export {default as partytown} from 'https://cdn.skypack.dev/@builder.io/partytown';
