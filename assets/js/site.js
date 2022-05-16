9af5af66cd3feba707188a94981bd7c5681b67fb
Paul <64870518+woodcock3@users.noreply.github.com>
2022-05-16 13:47:29 +0100

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,5 +1,2 @@
 import Alpine from 'https://cdn.skypack.dev/alpinejs';
 Alpine.start();
-
-import { partytownSnippet } from 'https://cdn.skypack.dev/@builder.io/partytown';
-const snippetText = partytownSnippet();
