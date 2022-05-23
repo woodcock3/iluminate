92cc06cca6a3efe8392978d68f7c06519542be7f
Paul <64870518+woodcock3@users.noreply.github.com>
2022-05-23 22:33:17 +0100

    Update _config.ts

--- _config.ts
+++ _config.ts
@@ -34,7 +34,7 @@ site
   .use(metas())
   .use(svgo())
   .use(basePath())
-  .loadData([".md"]) 
+  .loadData([".md", ".liquid"])
   .loadAssets([".js"], textLoader)
   // .process([".js"], processor.js)
   .use(slugifyUrls({ alphanumeric: false }))
