13fc49356d087fce4007686416e85030853191d0
Paul <64870518+woodcock3@users.noreply.github.com>
2022-05-25 01:19:43 +0100

    Update _liquidLoader.ts

--- _liquidLoader.ts
+++ _liquidLoader.ts
@@ -1,6 +1,6 @@
-import { LiquidEngine } from "https://deno.land/x/lume@v1.9.0/plugins/liquid.ts";
+import { Options } from "https://deno.land/x/lume@v1.9.0/plugins/liquid.ts";
 
 async function liquidLoader(path) {
   const content = await Deno.readTextFile(path);
-  return LiquidEngine(content);
+  return Options(content);
 }
